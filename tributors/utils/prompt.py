39d43d9953f87fbce3db34315263027cbdbbef59
Yaroslav Halchenko <debian@onerussian.com>
2024-05-15 15:06:32 -0400

    Remove some unnecessary python 2 compat cludges etc

--- tributors/utils/prompt.py
+++ tributors/utils/prompt.py
@@ -1,31 +1,13 @@
 """
 
 Copyright (C) 2020-2022 Vanessa Sochat.
+              2024      Yaroslav O. Halchenko
 
 This Source Code Form is subject to the terms of the
 Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
 with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 """
-
-
-enter_input = getattr(__builtins__, "raw_input", input)
-
-
-def request_input():
-    """Wait for the user to input some string, optionally with multiple lines."""
-    lines = []
-
-    # The message can be multiple lines
-    while True:
-        try:
-            line = enter_input()
-        except EOFError:
-            break
-        if line:
-            lines.append(line)
-
-    return "\n".join(lines)
 
 
 def choice_prompt(prompt, choices, choice_prefix=None, multiple=False):
@@ -41,15 +23,12 @@ def choice_prompt(prompt, choices, choice_prefix=None, multiple=False):
     choice = None
     print(prompt)
 
-    # Support for Python 2 (raw_input)
-    get_input = getattr(__builtins__, "raw_input", input)
-
     if not choice_prefix:
         choice_prefix = "/".join(choices)
     message = "[%s] : " % (choice_prefix)
 
     while choice not in choices:
-        choice = get_input(message).strip()
+        choice = input(message).strip()
 
         # If multiple allowed, add selection to choices if includes all valid
         if multiple is True:
@@ -58,3 +37,5 @@ def choice_prompt(prompt, choices, choice_prefix=None, multiple=False):
                 choices.append(choice)
         message = "Please enter a valid option in [%s]" % choice_prefix
     return choice
+
+
